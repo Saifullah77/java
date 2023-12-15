26eb0997420b40f117e11a8ea8d12f72b1675e7d
Md. Saifullah <110691640+Saifullah77@users.noreply.github.com>
2023-12-15 19:33:44 +0600

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,10 @@
+public class Count{
+    public static void main(String [] args){
+            int n = 7;
+            System.out.println(numberOfMatches(n));
+
+    }
+     public static int numberOfMatches(int n) {
+        return n-1;
+    }
+}
